08e7c1419c1bc7bcfe4c8b2690834a32666768c4
Irina Babenko <babenko8irina@gmail.com>
2025-04-13 19:17:10 +0300

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+    <main className="min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold text-red-600">Hello, Tailwind!</h1>
     </main>
   );
